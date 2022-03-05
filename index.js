6a8569c8d9bbe9a5f452abe5e44e44f48ebcfa77
Wilson Gaturu <wilsongaturu@gmail.com>
2022-03-05 16:08:25 +0300

    small changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const  http = require('http');
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.end('Hello Worldsh\n');
 })
 .listen(8081);
 
